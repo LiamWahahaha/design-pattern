1e462367bb1914082dedb82e5599de4ac876d178
liam wang <liliwang@cs.stonybrook.edu>
2019-12-28 18:52:59 -0500

    Add a description for abstract factory (ref: refactoring guru)

--- creational-patterns/abstract-factory/conceptual-example/main.py
+++ creational-patterns/abstract-factory/conceptual-example/main.py
@@ -1,6 +1,21 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
+# Abstract Factory solves the problem of creating entire product families
+# without specifying their concrete classes. It defines an interface for
+# creating all distinct products but leaves the actual product creation
+# to concrete factory classes. Each factory type corresponds to a certain
+# product variety.
+
+# The client code calls the creation methods of a factory object instead of
+# creating products directly with a constructor call (new operator). Since
+# a factory factory cooresponds to a single product variant, all its products
+# will be compatible.
+
+# Client code works with factories and products only through their abstract
+# interfaces. It allows the same client code to work with different products.
+# You just create a new concrete factory class and pass it to the client code.
+
 class AbstractFactory(ABC):
     """
     The Abstract Factory interface declares a set of methods that return
